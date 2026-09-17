b6be188bd9b76e8a00b603ff35d71b5f160f9312
frede <frededv@outlook.dk>
2026-09-17 10:55:26 +0200

    Alerts added to claryfy user error2

--- src/components/ProtectedRoute/PathRedirector.jsx
+++ src/components/ProtectedRoute/PathRedirector.jsx
@@ -18,14 +18,18 @@ export default function ProtectedRoute() {
 
   
   if (!token) {
+    Alert("Du skal være logget ind for at få adgang til denne side. Log ind for at fortsætte.");
     return <Navigate to="/auth/login" replace />;
+    
   }
 
   if (tokenIsValid === null) {
+    Alert("Vi tjekker om du er logget ind. Hvis du ikke bliver viderestillet, så prøv at logge ind igen.");
     return null; // or a loading spinner, while we wait for the check
   }
 
   if (!tokenIsValid) {
+    Alert("Din session er udløbet. Log ind igen for at fortsætte.");
     return <Navigate to="/auth/login" replace />;
   }
 
